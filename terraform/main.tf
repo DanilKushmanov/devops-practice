899fa2a687f31c30d4140f71081ffd8a09402cab
Danil Kushmanov <danil.kushmanov@gmail.com>
2024-01-27 14:59:25 +0300

    поднял машину на 2 ядра 4 гига хдд убунту, прописал всё в main.tf, должно работать если чета наебнется - я не виноват хд

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,5 +8,59 @@ terraform {
 }
 
 provider "yandex" {
-  zone = "<default_availability_zone>"
+  zone = "ru-central1-a"
+  token = "y0_AgAAAAAJ89LzAATuwQAAAADXw9Jg9c02CFcWRByVNmiLPNOJSPw0xPA"
+  cloud_id = "b1gc7onttnadnivcrdpf"
+  folder_id = "b1g43aq7nnnac7rsogvm"
+}
+
+resource "yandex_vpc_network" "default" {
+  name = "test_network"
+}
+
+resource "yandex_vpc_subnet" "test-subnet-a" {
+  v4_cidr_blocks = ["192.168.0.0/24"]
+  zone = "ru-central1-a"
+  network_id = "${yandex_vpc_network.default.id}"
+}
+
+resource "yandex_vpc_address" "site-addr" {
+  name = "ext-ip"
+
+  #external_ipv4_address { 
+    #
+  #}
+}
+
+resource "yandex_compute_disk" "vm-hdd" {
+  name = "hdd"
+  type = "network-hdd"
+  zone = "ru-central1-a"
+  size = 10
+
+  image_id = "fd8vljd295nqdaoogf3g"
+}
+
+resource "yandex_compute_instance" "vm-site" {
+  name = "site"
+  zone = "ru-central1-a"
+
+  resources {
+    cores = 2
+    memory = 4
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.vm-hdd.id
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.test-subnet-a.id}"
+    nat = true
+  }
+
+}
+
+output "site-addr" { # не выдал внешний айпи, надо это отработать
+  value       = "${yandex_compute_instance.vm-site.network_interface[0].nat_ip_address}"
 }
